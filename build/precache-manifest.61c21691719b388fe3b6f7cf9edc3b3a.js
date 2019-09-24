fd5d06951726cd71655c454486a1ba77c35664ed
bikundo <allanbmageto@gmail.com>
2019-09-24 23:09:48 +0300

    Static sever added and database details too

---
+++ build/precache-manifest.61c21691719b388fe3b6f7cf9edc3b3a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/argon-dashboard-react/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "605594e8b5982e4e149e",
+    "url": "/argon-dashboard-react/static/css/main.fab8be06.chunk.css"
+  },
+  {
+    "revision": "c722ac7e99944fb24708",
+    "url": "/argon-dashboard-react/static/js/runtime~main.e1a1abba.js"
+  },
+  {
+    "revision": "da5ebc950e55cf3b1d51",
+    "url": "/argon-dashboard-react/static/js/2.ed8c7414.chunk.js"
+  },
+  {
+    "revision": "508b1a33eb851b5af899b98517c15b5e",
+    "url": "/argon-dashboard-react/static/media/argon-react.508b1a33.png"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/argon-dashboard-react/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/argon-dashboard-react/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/argon-dashboard-react/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/argon-dashboard-react/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/argon-dashboard-react/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/argon-dashboard-react/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/argon-dashboard-react/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/argon-dashboard-react/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/argon-dashboard-react/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "605594e8b5982e4e149e",
+    "url": "/argon-dashboard-react/static/js/main.a7167009.chunk.js"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/argon-dashboard-react/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/argon-dashboard-react/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/argon-dashboard-react/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/argon-dashboard-react/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/argon-dashboard-react/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/argon-dashboard-react/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/argon-dashboard-react/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/argon-dashboard-react/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/argon-dashboard-react/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/argon-dashboard-react/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/argon-dashboard-react/static/media/team-4-800x800.23007132.jpg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/argon-dashboard-react/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/argon-dashboard-react/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "0f722fe57a261009e05248121040102b",
+    "url": "/argon-dashboard-react/index.html"
+  }
+];
