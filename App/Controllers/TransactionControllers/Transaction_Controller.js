9e35eb438f1d1ea3884edd20bc034b0d6c876f49
Allan Mageto <allanbmageto@gmail.com>
2020-03-21 17:46:19 +0300

    Confirmation of transactions done

--- App/Controllers/TransactionControllers/Transaction_Controller.js
+++ App/Controllers/TransactionControllers/Transaction_Controller.js
@@ -4,24 +4,44 @@ const TransactionModel = require("./../../Models/Transaction");
 
 module.exports = {
   createNewTransaction: (req, res) => {
-    TransactionModel.create(req.body).then(response => {
-      res.status(200).send(response);
-    }).catch(error => {
-        res.status(500).send(error)
-    })
+    TransactionModel.create(req.body)
+      .then(response => {
+        res.status(200).send(response);
+      })
+      .catch(error => {
+        res.status(500).send(error);
+      });
   },
-  getAllTransactions: (req,res) => {
-    TransactionModel.findAll().then(response =>{
-      res.status(200).send(response)
-    }).catch(error => {
-      res.status(500).send(error)
-    })
+  getAllTransactions: (req, res) => {
+    TransactionModel.findAll()
+      .then(response => {
+        res.status(200).send(response);
+      })
+      .catch(error => {
+        res.status(500).send(error);
+      });
   },
-  getUserTransactions: (req,res)=>{
-    TransactionModel.findAll().then(response =>{
-      res.status(200).send(response)
-    }).catch(error => {
-      res.status(500).send(error)
-    })
+  getUserTransactions: (req, res) => {
+    TransactionModel.findAll()
+      .then(response => {
+        res.status(200).send(response);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  },
+  confirmTransaction: (req, res) => {
+    if (!(req.user.UserCategoryId === 2)) {
+      res.status(401).send();
+    } else {
+      const verifiedById = { verifiedById: req.user.id };
+      Object.assign(req.body, verifiedById);
+      TransactionModel.update(req.body, { where: { id: req.body.id } })
+        .then(response => {
+          console.log(response);
+          res.send(response);
+        })
+        .catch(err => res.status(500).send(err));
+    }
   }
 };
--- App/Router/TransactionRoutes/Transaction_routes.js
+++ App/Router/TransactionRoutes/Transaction_routes.js
@@ -8,4 +8,6 @@ module.exports = app => {
    app.route("/transactions/getalltransactions").get(TransactionsController.getAllTransactions)
 
    app.route("/transactions/getusertransactions/:userid").get(TransactionsController.getUserTransactions)
+
+   app.route("/transactions/confirmtransaction").post(TransactionsController.confirmTransaction)
 }
