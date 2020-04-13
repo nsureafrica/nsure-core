ae0a461dda4d0a46e2df1c6bd34950a0d460780d
Allan Mageto <allanbmageto@gmail.com>
2020-04-13 22:25:17 +0300

    moved things to query

--- App/Controllers/DownloadControllers/download.js
+++ App/Controllers/DownloadControllers/download.js
@@ -7,7 +7,7 @@ const MotorPolicyModel = require("./../../Models/motor_policy")
 const ClaimModel = require("./../../Models/Claim")
 module.exports = {
     downloadTravelDocuments: async (req,res)=>{
-         const travelPolicyDocuments = await TravelPolicyModel.findOne({where: {id:req.body.travelPolicyId}})
+         const travelPolicyDocuments = await TravelPolicyModel.findOne({where: {id:req.query.travelPolicyId}})
         var nationalIdArray = travelPolicyDocuments.nationalId.split(",");
         var passportArray = travelPolicyDocuments.passport.split(",")
         var filesArray = [...nationalIdArray, ...passportArray];
@@ -16,7 +16,7 @@ module.exports = {
     },
 
     downloadMotorDocuments: async (req,res)=> {
-        const MotorPolicyModelResponse = await MotorPolicyModel.findOne({where: {id:req.body.motorPolicyId}}) 
+        const MotorPolicyModelResponse = await MotorPolicyModel.findOne({where: {id:req.query.motorPolicyId}}) 
         var kraPinArray = MotorPolicyModelResponse.kraPin.split(",");
         var idNumberArray = MotorPolicyModelResponse.idNumber.split(",");
         var logbookPathArray = MotorPolicyModelResponse.logbookPath.split(",");
@@ -25,7 +25,7 @@ module.exports = {
     },
 
     downloadClaimDocuments: async (req,res)=> {
-        const ClaimModelResponse = await ClaimModel.findOne({where: {id:req.body.claimId}})
+        const ClaimModelResponse = await ClaimModel.findOne({where: {id:req.query.claimId}})
         var claimPhotosArray =  ClaimModelResponse.claimForms.split(",");
         var claimDocs = ClaimModelResponse.claimPhotos.split(",");
         var filesArray = [...claimPhotosArray, ...claimDocs];
