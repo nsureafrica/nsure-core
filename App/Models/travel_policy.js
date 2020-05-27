a0f977dc4a9d003f8c94f439bb45b7f26b07e29e
Allan Mageto <allanbmageto@gmail.com>
2020-05-27 13:05:26 +0300

    removed fields

--- App/Models/travel_policy.js
+++ App/Models/travel_policy.js
@@ -7,49 +7,6 @@ const Bill = require("./Bill");
 const TravelPolicyPlan = require("./travel_policy_plans")
 
 const TravelPolicy = sequelizeConnection.define("TravelPolicy", {
-  //MEDICAL EXPENSES
-  medicalExpenses: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-    defaultValue: 0
-  },
-  followUpTreatmentInCountryOfResidence: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false
-  },
-  medicalEvaluationExpenses: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-    defaultValue: 0
-  },
-  repartriationOfMortalRemains: {
-    type: Sequelize.BOOLEAN
-  },
-  accompaniedByFamilyMember: {
-    type: Sequelize.BOOLEAN,
-    allowNull: true
-  },
-
-  //TRAVEL ASSISTANCE SERVICES
-
-  prematureReturn: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false
-  },
-  legalAssistance: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false
-  },
-  // LUGGAGE,TRADE SAMPLES OR PERSONAL EFFECTS
-
-  lossOrTheft: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false
-  },
-  luggageDelay: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false
-  },
   destination: {
     type: Sequelize.STRING,
     allowNull: false
